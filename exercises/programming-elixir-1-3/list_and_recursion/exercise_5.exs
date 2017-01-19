fe0d3c6fcd4a05afcfc021317e6886f2611fb1a3
Bruno Malvestuto <bruno.malvestuto@gmail.com>
2017-01-19 11:35:43 +0000

    Working implementation for exercise_5

--- exercises/programming-elixir-1-3/list_and_recursion/exercise_5.exs
+++ exercises/programming-elixir-1-3/list_and_recursion/exercise_5.exs
@@ -1,7 +1,7 @@
 defmodule MyEnum do
+
   def all?(list, fun \\ fn(_) -> true end )
   def all?([], _), do: true
-
   def all?([element | tail], fun) do
     if element != nil && fun.(element)  do
       all?(tail, fun)
@@ -18,9 +18,7 @@ defmodule MyEnum do
   end
 
   def filter(list, fun), do: _filter(list, fun, [])
-
   defp _filter([], _, acc), do: acc
-
   defp _filter([element | tail], fun, acc) do
 
     if fun.(element) do
@@ -28,7 +26,19 @@ defmodule MyEnum do
     else
       _filter(tail, fun, acc )
     end
-
   end
 
+
+  def split(list, count), do: _split(list, count, [])
+
+  defp _split(list, 0, acc), do: { Enum.reverse(acc), list }
+  defp _split([], _, acc), do:  { Enum.reverse(acc), [] }
+  defp _split([head | tail], count, acc)  do
+    _split(tail, count-1, [ head | acc ] )
+  end
+
+  def take(list, count), do: _take(list, count, [])
+  defp _take(_, 0, acc), do: Enum.reverse(acc)
+  defp _take([], _, acc), do: Enum.reverse(acc)
+  defp _take([head | tail], count, acc), do: _take(tail, count-1, [ head | acc])
 end
